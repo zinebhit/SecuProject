7629bdfc35e8574231e8dcdcf9a803f1f4ef2fe1
Hitait Zineb <zineb.hitait@insa-cvl.fr>
2021-10-07 12:20:35 +0200

    [fix]: added .gitignore and test code *from today's session tests

---
+++ Tests/test1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = pd.read_csv('groundTruth.csv') # rename as how your groundtruth is 
+data1 = data.to_numpy()
+
+copy = open("copy.csv","w")
+copy.write(data.to_string())
+copy.close()
+
+#small column additon 
+
+# print(df.shape)
+# new_column = pd.Series(['1','2'],name="extra_column")
+# df.update(new_column)
+
+#big column modification
--- python/test1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('groundTruth.csv')
-data1 = data.to_numpy()
-
-copy = open("copy.csv","w")
-copy.write(data.to_string())
-copy.close()
