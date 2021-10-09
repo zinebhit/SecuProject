f4c494597d0dc4ace9a6d23cf0becbef4d9e577d
LE Quoc Dat <quocdat.le.insacvl@gmail.com>
2021-10-09 17:17:51 +0200

    [feat]: generate small data set

--- Tests/C++/main.cpp
+++ Tests/C++/main.cpp
@@ -2,11 +2,19 @@
 // Created by nightfury on 10/8/21.
 //
 #include "DAZC.cpp"
+
 using namespace std;
 
 int main() {
     // To count number of lines of data set, use : wc -l name_data_set.csv
-    ULDataFrame df = dazc::read("/home/nightfury/CLionProjects/bdd.csv", 34551849);
+//    ULDataFrame df = dazc::read("/home/nightfury/CLionProjects/bdd.csv", 34551849);
+
+    // Generate test file :
+    dazc::generate_small_data_set("/home/nightfury/CLionProjects/bdd.csv",
+                                  "/home/nightfury/CLionProjects/small_data_set.csv", 200);
+    // For test's purpose
+//    ULDataFrame df = dazc::read("/home/nightfury/CLionProjects/bdd.csv", 34551849);
+//    dazc::write(df, "/home/nightfury/CLionProjects/output.csv");
     return 0;
 }
 
