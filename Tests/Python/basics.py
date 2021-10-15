6c41dab69aaa140c8d425da5899e42f2a54124e9
Christine Khalil <christinekh14@gmail.com>
2021-10-15 13:18:16 +0200

    [feat]: clusters' creation

--- Tests/Python/basics.py
+++ Tests/Python/basics.py
@@ -1,4 +1,7 @@
+from os import close
 import pandas as pd
+
+debug = 1
 
 def readCSV(filename):
     column_names = ["ID", "DateTime", "X", "Y"]
@@ -19,11 +22,39 @@ def switchLines(fileName, index1, index2):
         Attention: indexing starts at 0.
     """
     dataframe = readCSV(fileName)
-    # print(dataframe.iloc[lambda x : x.index == index1])
-    # print(dataframe.iloc[lambda x : x.index == index2])
     temp = dataframe.iloc[index1]
     dataframe.iloc[lambda x : x.index == index1] = dataframe.iloc[lambda x : x.index == index2]
     dataframe.iloc[lambda x : x.index == index2] = temp
     dataframe.to_csv(fileName, index = False, sep='\t', header=None)
+    
+    if (debug):
+        print("Switching line:\n",dataframe.iloc[lambda x : x.index == index1],"\n With the line:\n",dataframe.iloc[lambda x : x.index == index2],"\n")
 
+def findClosest(id,ids):
+    """
+        This function is going to help us find, for a specific id, the ids of the people whose position is the closest to his and remove the ids from 
+        our initial ids' list for optimization and to prevent repetition. 
+        id: id of the specific person 
+        ids: list of ids present in our ground truth file
+    """
+    #Access the element using loc with the ID attribute for optimization : df.loc[df['ID'] == 1] or with the coordinates attributes e.g: df.loc[df['X'] == x1]
+    
+    print("Id: ", id)
+    ids.remove(id)
+    df = readCSV("groundTruth.csv")
+    person = df.loc[df['ID'] == id]
+    x = person.iloc[0,2]
+    y = person.iloc[0,3]
+    closePeople = df.loc[(round(df['Y'],5) == round(y,5)) & (df['ID'] != id) & (round(df['X'],2) == round(x,2))]
+    closeIDs = [id]
+    for i in range(len(closePeople)):
+        if(len(closeIDs) == 11):
+            break
+        if((closePeople.iloc[i,0] not in closeIDs) and (closePeople.iloc[i,0] in ids)):
+            closeIDs.append(closePeople.iloc[i,0])
+            ids.remove(closePeople.iloc[i,0])
+
+    if(debug):
+        print("Id: ", id, "\nclosest: \n",closeIDs)
+        print("IDs left: \n",ids,"\n\n\n")
 
--- Tests/Python/testBasics.py
+++ Tests/Python/testBasics.py
@@ -1,4 +1,21 @@
-from basics import readCSV, switchLines
+from basics import findClosest,readCSV
+
+
+df = readCSV('groundTruth.csv')
+
+#We should define a list containing all the ids of our database seeing that the ids are random 
+#Since we can't convert directly a Series Object to a set we do the following 
+
+ids = list(set(df['ID'].tolist()))
+ids_copy = list(set(df['ID'].tolist()))
 
 #print(readCSV("smallBDD.csv"))
-switchLines("smallBDD.csv",0,4)
+#switchLines("smallBDD.csv",0,4)
+
+#In total we got 14 clusters: 249.94 secs to generate we have to optimize it later on
+
+for id in ids_copy:
+    if id in ids:
+        findClosest(id,ids)
+
+print(ids)
