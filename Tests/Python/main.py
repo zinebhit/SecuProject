a3b2732fc211a01b09a13ff83b6e36977014bf10
Agathe Leca <agathe.leca@hotmail.fr>
2021-11-12 13:52:15 +0100

    [modif] test sacrificeHour

--- Tests/Python/main.py
+++ Tests/Python/main.py
@@ -10,14 +10,15 @@ if __name__ == '__main__':
     column_names = ["ID", "DateTime", "X", "Y"]
     dtypes = {'ID': 'str', 'DateTime': 'str', 'X': 'float64', 'Y': 'float64'}
     parse_dates = ['DateTime']
-    df = pd.read_csv('data_set.csv', sep='\t', header=None, names=column_names, dtype=dtypes, parse_dates=parse_dates)
+    df = pd.read_csv('entree.csv', sep='\t', header=None, names=column_names, dtype=dtypes, parse_dates=parse_dates)
     #df = pd.read_csv('smallBDD.csv', sep='\t', header=None, names=column_names, dtype=dtypes, parse_dates=parse_dates)
     #defense = Defense()
     #df_id_anonymized = defense.disturb_id(df)
     #print(df_id_anonymized)
 
     # test sacrificeHour
-    df = hour_sacrifice(df, {"column": "DateTime", "params": (2, 100, 100)});
-    metrics = Metrics(df, df)
+    df2 = df.copy()
+    hour_sacrifice(df2, {"column": "DateTime", "params": (2, 100, 100)});
+    metrics = Metrics(df2, df)
     metrics.calculate_all()
     # {"name": "sacrificeHour", "options": {"column": "date", "params": (2, 100, 100)}},
